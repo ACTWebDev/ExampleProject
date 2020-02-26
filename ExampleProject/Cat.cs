a25574fb407c29b84222edf75626b4b70de08fc6
Steve-Rossiter <53451641+Steve-Rossiter@users.noreply.github.com>
2020-02-26 11:35:35 -0500

    Added the cat class.

---
+++ ExampleProject/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleProject
+{
+    class Cat : Animal
+    {
+    }
+
+    public void Meow()
+    {
+
+        Console.WriteLine("The cat goes Meow!");
+    }
+}
