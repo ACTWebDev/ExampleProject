3cf3caec4300d9377228d560a09c1dd6177a5ebe
Steve-Rossiter <53451641+Steve-Rossiter@users.noreply.github.com>
2020-02-26 11:27:26 -0500

    Added animal class.

---
+++ ExampleProject/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleProject
+{
+    class Animal
+    {
+        public string name;
+        public int age;
+        public string typeOfAniaml;
+
+        public Animal()
+        {
+
+        }
+    }
+}
