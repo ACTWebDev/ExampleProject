114be8f690cb933293cd8db639217df65f444662
Steve-Rossiter <53451641+Steve-Rossiter@users.noreply.github.com>
2020-02-26 11:10:08 -0500

    Add project files.

---
+++ ExampleProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExampleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int myNumber = 2;
+            int someNumber = 2;
+            int result = myNumber + someNumber;
+            Console.WriteLine(result);
+
+        }
+    }
+}
